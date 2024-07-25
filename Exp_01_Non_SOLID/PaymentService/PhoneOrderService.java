624c19cea8fb2053c2bf64b381cbb949b3075b71
behzadnabawi <behzadnabawi@gmail.com>
2024-07-25 13:25:40 +0330

    Create PhoneOrderService and onSiteOrderRegister

---
+++ Exp_01_Non_SOLID/PaymentService/PhoneOrderService.java
@@ -0,0 +1,8 @@
+package Step_1_Non_SOLID.PaymentServices;
+
+public class PhoneOrderService implements OrderService{
+    @Override
+    public void onSiteOrderRegister(String customerName) {
+        //Empty Body!
+    }
+}
